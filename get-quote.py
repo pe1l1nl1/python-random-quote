79aa64dd1e85659c7ec86a49c5f9673c87c30106
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2022-02-18 23:51:51 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
+import random
 def lychee():
   print("Keep it logically awesome.")
   python3 get-guote.py
+  
   f = open("quotes.txt")
   quotes = f.readlines()
+  
   f.close()
-
-  print(quotes[13])
+  last = 13. 
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   lychee()
